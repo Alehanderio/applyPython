d2c4b966e61929d72a8cfa6216c36becf4fb3868
Alehanderio <dryha.oleksandr@gmail.com>
2024-03-11 13:24:51 +0200

    task 3 done

---
+++ lab2/task3.py
@@ -0,0 +1,10 @@
+check_point = int(input("Enter an int to be compared to: "))
+lenTemp = int(input("Enter size of an array: "))
+int_array = [lenTemp]
+print("Fill the array: ")
+for i in range(lenTemp):
+    int_array.append(int(input("Enter next int: ")))
+
+res = [num for num in int_array if num > check_point]
+
+print(res)
